9a4c8de1cfae9ef56d90a769fa3ebf601f2fde43
Susmita Paul <137711194+susmita413@users.noreply.github.com>
2024-03-31 05:59:42 +0600

    Add files via upload

---
+++ Phitron__Laptop/1st/ex1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+ int a,b ;
+ char p;
+ 
+ //printf("Hello");
+ //scanf("%d%c %d%c",&a,&p,&b,&p);
+ //scanf("%d%% %d%%",&a,&b) ;
+ scanf("%d x %d y",&a,&b) ;
+
+ printf("A= %dx \n B= %dy",a,b);
+ //printf("A= %d%% \n B= %d%%",a,b);
+ return 0;
+}
