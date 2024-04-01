d32dcbde28a6bc4cfc0c8fbe1ff7d0e1d505de0a
Susmita Paul <137711194+susmita413@users.noreply.github.com>
2024-04-01 10:10:54 +0600

    Add files via upload

---
+++ setup__/hello.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+     int a;
+     scanf("%d",&a);
+     printf("HELLO !! ");
+     printf("\n A= %d\n", a);
+     printf("INPUT, OUTPUT text file khola mandetory na.");
+     return 0 ;
+}
