6976aa64ae2dcaa5de964ad5ff2f150c8a0df956
Susmita Paul <137711194+susmita413@users.noreply.github.com>
2024-04-19 23:35:21 +0600

    Add files via upload

---
+++ module 6-5_practiceProb/A_Timon_andPumba.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+    int a,b ;
+    scanf("%d %d",&a,&b);
+    if(a-b >0) printf("%d",a-b);
+    if(a-b<=0) printf("%d",0);
+    return 0 ;
+}
---
+++ module 6-5_practiceProb/D_aliBabaAndPuzzel.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+    long long int a,b,c,d ;
+    scanf("%lld %lld %lld %lld",&a,&b,&c,&d);
+    if(d== a+b-c || d== a+b*c || d== a-b+c || d== a-b*c || d== a*b+c|| d== a*b-c){
+        printf("YES\n");
+    }
+    else printf("NO\n");
+    return 0 ;
+}
