73181f5da43c70bd2d6de83c7fe48c5e897ebcf5
Susmita Paul <137711194+susmita413@users.noreply.github.com>
2024-04-21 19:05:55 +0600

    Update G_katryoshka.c

--- module 6-5_practiceProb/G_katryoshka.c
+++ module 6-5_practiceProb/G_katryoshka.c
@@ -9,29 +9,30 @@
 
 #include<stdio.h>
 int main(){
-    
     long long int n,m,k;
     scanf("%lld %lld %lld",&n,&m,&k);
-    int count=0 ;
+    long long int count=0 ;
     
-    if(n<=m && n<=k){
-        count = n ; n=0 ;  m= m-n ;  k = k-n ;
+    if(n<m && n<k){
+        count = n ;  m= m-n ;  k = k-n ;  n=0 ;
     }
     else if(m<n && m<k){
-        count = m ; m=0 ;  n= n-m ;  k = k-m ;
+        count = m ; n= n-m ;  k = k-m ; m=0 ;
     }
     else {
-        count = k; k=0;  m=m-k ; n=n-k ;
+        count = k;  m=m-k ; n=n-k ; k=0;
     }
-
+ 
+    long long int count2 =0 ;
     if(m==0){
-        if(k*2 <=n) count = count+k;
-        else if(k*2 >n){
-            int half = n/2 ;
-            count =count + half ;
-        }
+            if((n/2)> k){
+               count2= k ;
+            } 
+            else{
+               count2 = n/2 ;
+            } 
     }
-
-    printf("%d\n",count);
+    printf("%lld",count+ count2);
     return 0;
+ 
 }
